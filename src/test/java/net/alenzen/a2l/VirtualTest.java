3e81edee6f59088ca5ed3efff33f4ce6cd98ecbf
Andreas Lenzen <git@lord-luncher.de>
2020-11-14 12:47:17 +0100

    added VIRTUAL tests

---
+++ src/test/java/net/alenzen/a2l/VirtualTest.java
@@ -0,0 +1,32 @@
+package net.alenzen.a2l;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import net.alenzen.a2l.Asap2FileTest.TestFile;
+
+public class VirtualTest {
+	private Asap2File file;
+	private Virtual virtual;
+
+	@BeforeEach
+	void initTestfile() throws IOException {
+		file = Asap2FileTest.getTestFile(TestFile.A);
+		virtual = file.getProject().getModules().get(0).getMeasurements().get(0).getVirtual();
+
+		assertNotNull(virtual);
+	}
+	
+	@Test
+	void testMeasurementReferences() {
+		String[] meas = new String[] {"meas_1", "meas_2"};
+		for(int i = 0; i < virtual.size(); i++) {
+			assertEquals(meas[i], virtual.get(i));
+		}
+	}
+}
