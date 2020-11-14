f4be9df2a33f918cb243f5474da5811da11973ea
Andreas Lenzen <git@lord-luncher.de>
2020-11-14 10:34:38 +0100

    added IF_DATA test

---
+++ src/test/java/net/alenzen/a2l/IfDataTest.java
@@ -0,0 +1,29 @@
+package net.alenzen.a2l;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import net.alenzen.a2l.Asap2FileTest.TestFile;
+
+public class IfDataTest {
+	private Asap2File file;
+	private IfData ifData;
+	
+	@BeforeEach
+	void initTestfile() throws IOException {
+		file = Asap2FileTest.getTestFile(TestFile.A);
+		ifData = file.getProject().getModules().get(0).getMeasurements().get(0).getIfDatas().get(0);
+
+		assertNotNull(ifData);
+	}
+	
+	@Test
+	void testContent() {
+		assertEquals("/begin IF_DATA i am not parsed yet /end IF_DATA", ifData.getContent());
+	}
+}
