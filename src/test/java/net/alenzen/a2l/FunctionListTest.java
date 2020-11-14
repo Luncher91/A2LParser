1b1bd69876feb4546c625bb157d559ba50762c00
Andreas Lenzen <git@lord-luncher.de>
2020-11-14 10:26:21 +0100

    added tests for FUNCTION_LIST

--- src/test/java/net/alenzen/a2l/FunctionListTest.java
+++ src/test/java/net/alenzen/a2l/FunctionListTest.java
@@ -1,5 +1,32 @@
 package net.alenzen.a2l;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import net.alenzen.a2l.Asap2FileTest.TestFile;
+
 public class FunctionListTest {
-	// TODO
+	private Asap2File file;
+	private FunctionList functionList;
+	
+	@BeforeEach
+	void initTestfile() throws IOException {
+		file = Asap2FileTest.getTestFile(TestFile.A);
+		functionList = file.getProject().getModules().get(0).getMeasurements().get(0).getFunctionList();
+
+		assertNotNull(functionList);
+	}
+	
+	@Test
+	void testEntries() {
+		String[] values = new String[] {"firstFunction", "secondFunction", "thirdFunction"};
+		for(int i = 0; i < functionList.size(); i++) {
+			assertEquals(values[i], functionList.get(i));
+		}
+	}
 }
