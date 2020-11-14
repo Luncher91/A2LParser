b29e0a7179d414870ab0192de37691ba10fc7035
Andreas Lenzen <git@lord-luncher.de>
2020-11-14 13:02:53 +0100

    added A2ML test

---
+++ src/test/java/net/alenzen/a2l/A2mlTest.java
@@ -0,0 +1,29 @@
+package net.alenzen.a2l;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import net.alenzen.a2l.Asap2FileTest.TestFile;
+
+public class A2mlTest {
+	private Asap2File file;
+	private A2ml a2ml;
+
+	@BeforeEach
+	void initTestfile() throws IOException {
+		file = Asap2FileTest.getTestFile(TestFile.A);
+		a2ml = file.getProject().getModules().get(0).getA2ml().get(0);
+
+		assertNotNull(a2ml);
+	}
+	
+	@Test
+	void testContent() {
+		assertEquals("/begin A2ML i am not parsed /end A2ML", a2ml.getContent());
+	}
+}
