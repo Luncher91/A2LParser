3dd240fdb70eb221cc3f8aca3e00882e99157f2a
Andreas Lenzen <git@lord-luncher.de>
2020-11-14 12:43:31 +0100

    added SYMBOL_LINK tests

---
+++ src/test/java/net/alenzen/a2l/SymbolLinkTest.java
@@ -0,0 +1,33 @@
+package net.alenzen.a2l;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import net.alenzen.a2l.Asap2FileTest.TestFile;
+
+class SymbolLinkTest {
+	private Asap2File file;
+	private SymbolLink symbolLink;
+
+	@BeforeEach
+	void initTestfile() throws IOException {
+		file = Asap2FileTest.getTestFile(TestFile.A);
+		symbolLink = file.getProject().getModules().get(0).getMeasurements().get(0).getSymbolLink();
+
+		assertNotNull(symbolLink);
+	}
+	
+	@Test
+	void testName() {
+		assertEquals("symbol name", symbolLink.getSymbolName());
+	}
+	
+	@Test
+	void testOffset() {
+		assertEquals(84, symbolLink.getOffset());
+	}
+}
