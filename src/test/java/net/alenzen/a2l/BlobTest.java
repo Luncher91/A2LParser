4df4f581657e2798dfceb8053bfce1c8d5801572
Andreas Lenzen <git@lord-luncher.de>
2020-11-22 10:43:10 +0100

    added test for BLOB

---
+++ src/test/java/net/alenzen/a2l/BlobTest.java
@@ -0,0 +1,29 @@
+package net.alenzen.a2l;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import net.alenzen.a2l.Asap2FileTest.TestFile;
+
+public class BlobTest {
+	private Asap2File file;
+	private Blob blob;
+
+	@BeforeEach
+	void initTestfile() throws IOException {
+		file = Asap2FileTest.getTestFile(TestFile.A);
+		blob = file.getProject().getModules().get(0).getBlobs().get(0);
+
+		assertNotNull(blob);
+	}
+
+	@Test
+	void testContent() {
+		assertEquals("/begin BLOB i am not parsed neither /end BLOB", blob.getContent());
+	}
+}
