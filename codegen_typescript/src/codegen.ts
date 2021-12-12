dc2c3f1855cd421b1cd0bc24531edb2f4e347472
andrlenz <git@lord-luncher.de>
2021-12-12 20:16:49 +0100

    generate typescript code base in jsonSchema instead of jar

--- codegen_typescript/src/codegen.ts
+++ codegen_typescript/src/codegen.ts
@@ -3,7 +3,14 @@ import fs from 'fs'
 import { compile } from 'json-schema-to-typescript'
 
 console.log("Generating JSON schema...");
-let jsonSchema: string = execSync("java -jar \"" + process.argv[2] + "\" -jsc").toString();
+let jsonSchema: string;
+
+if(process.argv[2].endsWith('.jar')) {
+  jsonSchema = execSync("java -jar \"" + process.argv[2] + "\" -jsc").toString();
+} else {
+  jsonSchema = fs.readFileSync(process.argv[2]).toString().replace(/^\uFEFF/, '');
+}
+
 console.log("Preparing JSON schema...");
 let parsedSchema = JSON.parse(jsonSchema);
 console.log("Start parsing...")
