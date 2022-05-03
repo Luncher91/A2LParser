4d2ae7574f45cdc70dafbfe003237683f847dcda
Andreas Lenzen <git@alenzen.net>
2022-05-03 20:45:24 +0200

    fix travis CI badge fix wrong assertion import

--- src/test/java/net/alenzen/a2l/validation/FormulaValidatorTest.java
+++ src/test/java/net/alenzen/a2l/validation/FormulaValidatorTest.java
@@ -1,8 +1,8 @@
 package net.alenzen.a2l.validation;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
